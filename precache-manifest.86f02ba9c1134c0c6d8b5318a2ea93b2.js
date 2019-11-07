a7d841a052d3d2b71c6c1b0ba48ed51c11fe84ee
Apoorv Sohal <apoorvsohal@protonmail.com>
2019-11-07 21:24:10 +0530

    Updates

---
+++ precache-manifest.86f02ba9c1134c0c6d8b5318a2ea93b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16170ebf62c0a06935a59d4e982b3528",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ecfb143344a815e301cd",
+    "url": "/monsters-rolodex/static/css/main.052ba470.chunk.css"
+  },
+  {
+    "revision": "6df4eb5567d793ed81f5",
+    "url": "/monsters-rolodex/static/js/2.975c5e1d.chunk.js"
+  },
+  {
+    "revision": "ecfb143344a815e301cd",
+    "url": "/monsters-rolodex/static/js/main.70b55378.chunk.js"
+  },
+  {
+    "revision": "f56f2e2c6d96b014d91f",
+    "url": "/monsters-rolodex/static/js/runtime-main.86190389.js"
+  }
+]);
